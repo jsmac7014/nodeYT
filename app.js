3d8fc70f3f65f9d831315b5dbaacfe95a4217f17
JungWoo Song <jsmac7014@gmail.com>
2017-11-12 05:13:46 +0900

    ejs

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var Youtube = require('youtube-node');
 var youTube = new Youtube();
 
-var router = require('./router/main')(app);
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -11,12 +10,10 @@ app.engine('html', require('ejs').renderFile);
 
 youTube.setKey('AIzaSyC5bwEuaf6FmJQGeQps5daPrY_3yg8Y2dc');
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
-})
 
-var word = '디즈니';
-var limit = 1;
+
+var word = 'macbook';
+var limit = 5;
 
 youTube.addParam('order', 'relevance');
 // youTube.addParam('safeSearch', 'strict');
@@ -26,27 +23,56 @@ youTube.addParam('regionCode', 'KR');
 youTube.addParam('type', 'video');
 youTube.addParam('videoCaption','closedCaption');
 
-youTube.search(word, limit, function(err, result){
-  // youTube.clearParams();
-  // youTube.clearParts();
-  if (err){
-    console.log(err);
-    return;
-  }
-  else{
-
-  }
-  console.log(JSON.stringify(result, null, 2));
-
-  var items = result['items'];
-  for (var i in items){
-    var item = items[i];
-    var title = item['snippet']['title'];
-    var videoID = item['id']['videoId'];
-
-    var url = "https://www.youtube.com/watch?v=" + videoID;
-    console.log("title: " + title);
-    console.log("URL: " + url);
-    console.log("--------");
-  }
+var server = app.listen(3000, function(req,res){
+    console.log("Express server has started on port 3000");
 });
+
+
+
+
+// console.log(JSON.stringify(newdata,null ,2));
+
+app.get('/',function(req,res){
+  res.render('search');
+});
+app.get('/results',function(req,res){
+  youTube.search(word, limit, function(err, result){
+      // youTube.clearParams();
+      // youTube.clearParts();
+      if (err){
+        console.log(err);
+        return;
+      }
+      // console.log(JSON.stringify(result, null, 2));
+      newdata = JSON.stringify(result, null, 2);
+      console.log(newdata);
+
+      items = result['items'];
+      for (var i in items){
+        item = items[i];
+        title = item['snippet']['title'];
+        videoID = item['id']['videoId'];
+        url = "https://www.youtube.com/watch?v=" + videoID;
+        console.log("title: " + title);
+        console.log("URL: " + url);
+        console.log("--------");
+      }
+      res.render('results',{
+          items: items,
+          vidTitle: item['snippet']['title'],
+      });
+    });
+});
+// res.end(ejs.render(data,{
+//   // var items = result['items'];
+//   // for (var i in items){
+//   //   var item = items[i];
+//   //   var title = item['snippet']['title'];
+//   //   var videoID = item['id']['videoId'];
+//   //
+//   //   // var url = "https://www.youtube.com/watch?v=" + videoID;
+//   //   // console.log("title: " + title);
+//   //   // console.log("URL: " + url);
+//   //   // console.log("--------");
+//   // }
+// }));
--- router/main.js
+++ router/main.js
@@ -1,9 +1,4 @@
 module.exports = function(app)
 {
-     app.get('/',function(req,res){
-        res.render('search.ejs')
-     });
-     app.get('/results',function(req,res){
-        res.render('results.ejs');
-    });
+
 }
